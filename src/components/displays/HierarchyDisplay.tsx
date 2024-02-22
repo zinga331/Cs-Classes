f3c6ae0aa6e01f5e3fef128a6cf41e3224f39ee8
Tess-H <tessington32@gmail.com>
2024-02-21 17:55:07 -0700

    changed css and some naming convetions in HierachyDisplay

--- src/components/displays/HierarchyDisplay.tsx
+++ src/components/displays/HierarchyDisplay.tsx
@@ -7,49 +7,48 @@ interface Props {
 }
 
 const HierarchyDisplay = (props: Props) => {
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
-  const [selectedSubcategory, setSelectedSubcategory] = useState<string | null>(
-    null
-  );
-  const [filteredQuotes, setFilteredQuotes] = useState<Quote[]>([]);
+  const [selectedMajor, setSelectedMajor] = useState<string | null>(null);
+  const [selectedCourse, setSelectedCourse] = useState<string | null>(null);
+  const [filteredDetails, setFilteredDetails] = useState<Quote[]>([]);
+
   useEffect(() => {
-    if (selectedCategory && selectedSubcategory) {
-      const newFilteredQuotes = props.quotes.quotes.filter(
+    if (selectedMajor && selectedCourse) {
+      const newFilteredDetails = props.quotes.quotes.filter(
         (quote) =>
-          quote.category === selectedCategory &&
-          quote.subcategory === selectedSubcategory
+          quote.category === selectedMajor &&
+          quote.subcategory === selectedCourse
       );
-      setFilteredQuotes(newFilteredQuotes);
+      setFilteredDetails(newFilteredDetails);
     }
-  }, [selectedCategory, selectedSubcategory, props.quotes]);
+  }, [selectedMajor, selectedCourse, props.quotes]);
 
   const handleCategoryClick = (category: string) => {
-    setSelectedCategory(category);
-    setSelectedSubcategory(null);
-    setFilteredQuotes([]);
+    setSelectedMajor(category);
+    setSelectedCourse(null);
+    setFilteredDetails([]);
   };
 
-  const handleSubcategoryClick = (subcategory: string) => {
-    setSelectedSubcategory(subcategory);
-    console.log(subcategory + " clicked");
+  const handleSubcategoryClick = (course: string) => {
+    setSelectedCourse(course);
+    console.log(course + " clicked");
   };
 
   const renderSubcategories = () => {
-    if (!selectedCategory) {
+    if (!selectedMajor) {
       return null;
     }
 
     const subcategoriesList = props.quotes.quotes
-      .filter((quote) => quote.category === selectedCategory)
+      .filter((quote) => quote.category === selectedMajor)
       .map((quote) => quote.subcategory);
 
     // Create a Set to remove duplicates
-    const uniqueSubcategories = Array.from(new Set(subcategoriesList));
+    const uniqueCourse = Array.from(new Set(subcategoriesList));
 
     return (
       <div className="subcategories-container">
-        <h2>Subcategories</h2>
-        {uniqueSubcategories.map((subcategory, index) => (
+        <h2>CS Courses :</h2>
+        {uniqueCourse.map((subcategory, index) => (
           <div key={index}>
             <button onClick={() => handleSubcategoryClick(subcategory)}>
               {subcategory}
@@ -61,18 +60,18 @@ const HierarchyDisplay = (props: Props) => {
   };
 
   const renderQuotes = () => {
-    if (!selectedCategory) {
+    if (!selectedMajor) {
       return null;
     }
     // iterate through filteredQuotes and display the quote and author
-    for (let i = 0; i < filteredQuotes.length; i++) {
-      console.log(filteredQuotes[i]);
+    for (let i = 0; i < filteredDetails.length; i++) {
+      console.log(filteredDetails[i]);
     }
 
     return (
       <div className="quotes-container">
-        <h2>Quotes</h2>
-        {filteredQuotes.map((quote, index) => (
+        <h2>Course Details :</h2>
+        {filteredDetails.map((quote, index) => (
           <div key={index}>
             <p>{quote.quote}</p>
             <p>- {quote.author}</p>
@@ -81,10 +80,11 @@ const HierarchyDisplay = (props: Props) => {
       </div>
     );
   };
+
   return (
     <div className="hierarchy-display-container">
       <div className="categories-container">
-        <h2>Categories</h2>
+        <h2>CS Majors :</h2>
         {Array.from(props.quotes.categories).map((category, index) => (
           <div key={index}>
             <button onClick={() => handleCategoryClick(category)}>
@@ -93,8 +93,14 @@ const HierarchyDisplay = (props: Props) => {
           </div>
         ))}
       </div>
+      <div className="vertical_break"></div>
       <div>{renderSubcategories()}</div>
-      <div>{renderQuotes()}</div>
+      {selectedCourse !== null && (
+        <>
+          (<div className="vertical_break"></div>
+          <div>{renderQuotes()}</div>)
+        </>
+      )}
     </div>
   );
 };
