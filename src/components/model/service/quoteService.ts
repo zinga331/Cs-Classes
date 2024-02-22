294b5a382cdb4a8567eb57b6c83c987682ca7e27
Christian Hall <christianjameshall@gmail.com>
2024-02-22 13:34:40 -0700

    add function to quote service to parse json courses and then log them within getQuotes

--- src/components/model/service/quoteService.ts
+++ src/components/model/service/quoteService.ts
@@ -1,7 +1,10 @@
 // quoteService.ts
+// import fs from 'fs';
+// import path from 'path';
 import quotes from '../quoteList.json';
 import { Quotes, Quote } from '../domain/quotes';
 import {Course} from '../domain/courses';
+import coursesJson from '../courses.json';
 
 export function getQuotes() {
     // iterate through the quotes from quoteList.json and create a new Quotes object
@@ -19,10 +22,41 @@ export function getQuotes() {
         subcategories: new Set(quotes.quotes.map((quote: any) => quote.subcategory)),
     };
     console.log(quotesList);
+    console.log(courses);
     return quotesList;
 }
-export function getCourses() {
-    let courses : Course[] = [];
-    // Your implementation here
-    return courses;
-}
+export const courses: Course[] = coursesJson.map((courseJson: any): Course => {
+    // Map each JSON object to the Course interface
+    return {
+      id: courseJson.id,
+      courseGroupId: courseJson.courseGroupId,
+      curriculumId: courseJson.curriculumId,
+      titleCode: courseJson.titleCode,
+      code: courseJson.code,
+      subjectCode: courseJson.subjectCode,
+      courseNumber: courseJson.courseNumber,
+      name: courseJson.name,
+      longName: courseJson.longName,
+      description: courseJson.description,
+      learningOutcomes: courseJson.learningOutcomes,
+      credits: {
+        creditHours: courseJson.credits.creditHours,
+        lectureHours: courseJson.credits.lectureHours,
+        labHours: courseJson.credits.labHours,
+      },
+      courseTypicallyOffered: courseJson.courseTypicallyOffered,
+      courseType: courseJson.courseType,
+      note: courseJson.note,
+      fee: courseJson.fee,
+      courseDependents: courseJson.courseDependents,
+      programDependents: courseJson.programDependents,
+      requisites: courseJson.requisites,
+      nonEnforcedPrerequisites: courseJson.nonEnforcedPrerequisites,
+      recommended: courseJson.recommended,
+      "9fzcu": courseJson["9fzcu"],
+      mXQpZ: courseJson.mXQpZ,
+      L95zy: courseJson.L95zy,
+      xq1Dp: courseJson.xq1Dp,
+      awavi: courseJson.awavi,
+    };
+  });
